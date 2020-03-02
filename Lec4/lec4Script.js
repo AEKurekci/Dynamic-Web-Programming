b592dc8b850018e23b091ce15a2740328a5a2093
AEKurekci <aliemrekurekci@gmail.com>
2020-03-02 17:12:57 +0300

    lec4

--- Lec4/lec4Script.js
+++ Lec4/lec4Script.js
@@ -1,3 +1,4 @@
+var students = new Array();
 function splitText(names, splitCharacter){
     var namesArray = names.split(splitCharacter);
     return namesArray;
@@ -10,3 +11,43 @@ function printOnebyOne(list, seperater){
     }
     return result;
 }
+
+class student{
+    constructor(name, grades){
+        this.name = name;
+        this.gradeList = grades;
+    }
+    ToString(){
+        return this.name + "'s grades are " + this.gradeList + " and average is " + this.Average();
+    }
+
+    GetGrades(){
+        return this.gradeList;
+    }
+
+    GetName(){
+        return this.name;
+    }
+
+
+
+    Average(){
+        var sum = 0;
+        for(var i = 0; i< this.gradeList.length; i++){
+            sum += parseInt(this.gradeList[i]);
+        }
+        return sum/this.gradeList.length;
+    }
+}
+
+/* class sample
+*class car{
+    var carname;
+    constructor(param){
+        this.carname = param;
+    }
+    methodName(){
+        return this.carname;
+    }
+}
+*/
